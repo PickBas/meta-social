8b8f387a13bc62ba24be60d23b10e394d6ff57ce
Andrew Piryazev <piryazev777@gmail.com>
2020-02-14 21:43:31 +0300

    Done all authorization without design

--- meta_social/meta_social_app/forms.py
+++ meta_social/meta_social_app/forms.py
@@ -1,8 +1,22 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 
 
 class SignUpForm(UserCreationForm):
     email = forms.EmailField(max_length=254, help_text='Required. Inform a valid email address.')
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password1', 'password2')
+
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+
+        if not User.objects.filter(email=email).exists():
+            return email
+        else:
+            raise forms.ValidationError("Email is already in use!")
+
 
 
