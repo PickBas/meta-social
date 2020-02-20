225d9542267fb142a3325144914171a5fac3a202
Andrew Piryazev <piryazev777@gmail.com>
2020-02-20 11:53:49 +0300

    model of user is ready

--- meta_social/meta_social_app/models.py
+++ meta_social/meta_social_app/models.py
@@ -1,3 +1,11 @@
 from django.contrib.auth.models import User
 from django.db import models
 
+
+class modelUser(models.Model):
+    user = models.ForeignKey(to=User, on_delete=models.CASCADE)
+    avatar = models.ImageField(upload_to='avatars/', null=True, blank=True, default='avatars/0.png')
+    country = models.CharField(null=True, max_length=60)
+    birth_day = models.DateField(null=True)
+    status = models.CharField(max_length=500, null=True)
+    last_time_online = models.DateTimeField(null=True)
