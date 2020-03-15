6e76c5cd24925915a0971154425987ec3d832e43
Сайед Кирилл <sayed.kirill@gmail.com>
2020-03-15 17:36:46 +0300

    Fixed pagename on login, sign up

--- meta_social/meta_social/settings.py
+++ meta_social/meta_social/settings.py
@@ -99,15 +99,15 @@ WSGI_APPLICATION = 'meta_social.wsgi.application'
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.postgresql_psycopg2',
-    #     'NAME': 'DB',
-    #     'USER': 'postgres',
-    #     'PASSWORD': 'postgres',
-    #     'HOST': 'db',
-    #     'PORT': '5432',
-    # },
     'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'DB',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'db',
+        'PORT': '5432',
+    },
+    'old_sqlite3': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
