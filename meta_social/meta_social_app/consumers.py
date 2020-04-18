f2471b43b41c11588938ead0428da46e775efb08
Сайед Кирилл <sayed.kirill@gmail.com>
2020-04-18 14:36:59 +0300

    Added docs for consumers.py

--- meta_social/meta_social_app/consumers.py
+++ meta_social/meta_social_app/consumers.py
@@ -1,3 +1,8 @@
+"""
+Consumers module
+"""
+
+
 import json
 from channels.generic.websocket import WebsocketConsumer
 from asgiref.sync import async_to_sync
@@ -8,7 +13,16 @@ from .models import Message, Chat
 
 class ChatConsumer(WebsocketConsumer):
 
-    def fetch_messages(self, room_name):
+    """
+    ChatConsumer class
+    """
+
+    def fetch_messages(self, room_name: str) -> None:
+        """
+        Fetching messages during connection to a server
+        :param room_name: room_id
+        :return: None
+        """
         chat = Chat.objects.get(id=int(room_name))
         messages = chat.messages.all()
         content = {
@@ -16,24 +30,37 @@ class ChatConsumer(WebsocketConsumer):
         }
         self.send(text_data=json.dumps(content))
 
-    def messages_to_json(self, messages):
+    def messages_to_json(self, messages: list) -> list:
+        """
+        Converting all messages to json
+        :param messages: messaegs
+        :return: list
+        """
         result = []
         for message in messages:
             result.append(self.message_to_json(message))
         return result
 
-    def message_to_json(self, message):
+    def message_to_json(self, message: Message) -> dict:
+        """
+        Convert message data to json
+        :param message: message
+        :return: dict
+        """
         return {
             'author': message.author.username,
             'message': message.message,
             'date': str(message.date)
         }
 
-    def connect(self):
+    def connect(self) -> None:
+        """
+        connecting to a server
+        :return: None
+        """
         self.room_name = self.scope['url_route']['kwargs']['room_name']
         self.room_group_name = 'chat_%s' % self.room_name
 
-        # Join room group
         async_to_sync(self.channel_layer.group_add)(
             self.room_group_name,
             self.channel_name
@@ -42,15 +69,23 @@ class ChatConsumer(WebsocketConsumer):
         self.accept()
         self.fetch_messages(self.room_name)
 
-    def disconnect(self, close_code):
-        # Leave room group
+    def disconnect(self, close_code) -> None:
+        """
+        Disconnect
+        :param close_code: code
+        :return: None
+        """
         async_to_sync(self.channel_layer.group_discard)(
             self.room_group_name,
             self.channel_name
         )
 
-    # Receive message from WebSocket
-    def receive(self, text_data):
+    def receive(self, text_data) -> None:
+        """
+        Receive messages and write message to db
+        :param text_data: json
+        :return: None
+        """
         text_data_json = json.loads(text_data)
         message = text_data_json['message']
         author_user = User.objects.get(id=int(text_data_json['author']))
@@ -62,7 +97,6 @@ class ChatConsumer(WebsocketConsumer):
         chat.messages.add(new_message)
         chat.save()
 
-        # Send message to room group
         async_to_sync(self.channel_layer.group_send)(
             self.room_group_name,
             {
@@ -71,11 +105,14 @@ class ChatConsumer(WebsocketConsumer):
             }
         )
 
-    # Receive message from room group
-    def chat_message(self, event):
+    def chat_message(self, event: dict) -> None:
+        """
+        Send a message to a socket
+        :param event: dict
+        :return: None
+        """
         message = event['message']
 
-        # Send message to WebSocket
         async_to_sync(self.send(text_data=json.dumps({
             'messages': message
         })))
