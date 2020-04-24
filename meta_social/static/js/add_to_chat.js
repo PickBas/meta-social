fc978a47dbf35d199f5ab25785a26c0e9aad667f
Сайед Кирилл <sayed.kirill@gmail.com>
2020-04-24 20:31:53 +0300

    Fixed removing wrong element

--- meta_social/static/js/add_to_chat.js
+++ meta_social/static/js/add_to_chat.js
@@ -6,7 +6,17 @@ function addToChat(e, link) {
             csrfmiddlewaretoken: csr_token,
         },
         success: function () {
-            e.target.parentNode.remove();
+            let to_add = e.target.parentNode.parentNode.parentNode;
+            if (to_add.tagName === 'DIV') {
+                to_add.remove();
+            } else {
+                to_add = e.target.parentNode.parentNode;
+                to_add.remove();
+            }
+            const amount_to_invite = document.getElementById('friend-list-modal').getElementsByTagName('div').length;
+            if (amount_to_invite == 0) {
+                $('#AddToConvModal').modal('hide');
+            }
         }
     })
 }
