166a28cde84cf9170178330d7ce5ebc8a878d59f
Кропотухин Алексей <nameies@yandex.ru>
2020-04-25 20:22:20 +0300

    Migrations fixed

--- meta_social/meta_social/settings.py
+++ meta_social/meta_social/settings.py
@@ -94,6 +94,7 @@ CHANNEL_LAYERS = {
     'default': {
         'BACKEND': 'channels_redis.core.RedisChannelLayer',
         'CONFIG': {
+            # "hosts": [('127.0.0.1', 6379)], 
             "hosts": [('redis', 6379)],
         },
     },
