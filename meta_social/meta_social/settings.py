f6d468c2888b79aa74ba1639a15f0cfbc503feea
Шипунов Всеволод <shipunov_stalk@mail.ru>
2020-05-06 16:38:39 +0300

    add view photo

--- meta_social/meta_social/settings.py
+++ meta_social/meta_social/settings.py
@@ -105,17 +105,9 @@ CHANNEL_LAYERS = {
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'DB',
-        'USER': 'postgres',
-        'PASSWORD': 'postgres',
-        'HOST': 'db',
-        'PORT': '5432',
-    },
-    'old_sqlite3': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    },
 }
 
 # Password validation
