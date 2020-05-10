dacb0a05435450b387b162fa0ebae915a4a29edd
Кропотухин Алексей <nameies@yandex.ru>
2020-05-10 23:29:16 +0300

    Added files preview, but cant't delete files

--- meta_social/static/js/messagesockets.js
+++ meta_social/static/js/messagesockets.js
@@ -42,7 +42,10 @@ document.querySelector('#chat-message-submit').onclick = function(e) {
     if (message.length) {
         let form_data = new FormData();
         form_data.append('csrfmiddlewaretoken', c_token);
-        form_data.append('images', $('#message_files')[0].files[0]);
+        
+        for (let i=0; i<$('#message_files')[0].files.length; i++) {
+            form_data.append('images', $('#message_files')[0].files[i])
+        }
 
         $.ajax({
             type: 'POST',
@@ -61,6 +64,7 @@ document.querySelector('#chat-message-submit').onclick = function(e) {
                 }));
 
                 $('#message_files').val('')
+                $('#gallery').empty()
             }
         })
 
@@ -70,4 +74,75 @@ document.querySelector('#chat-message-submit').onclick = function(e) {
 
 function triggerInput() {
     $('#message_files')[0].click()
+    $('#gallery').empty()
+}
+
+let dropArea = document.getElementById('drop-area')
+
+;['dragenter', 'dragover', 'dragleave', 'drop'].forEach(eventName => {
+    dropArea.addEventListener(eventName, preventDefaults, false);
+});
+
+function preventDefaults(e) {
+    e.preventDefault();
+    e.stopPropagation()
+}
+
+;['dragenter', 'dragover'].forEach(eventName => {
+    dropArea.addEventListener(eventName, highlight, false)
+})
+
+;['dragleave', 'drop'].forEach(eventName => {
+    dropArea.addEventListener(eventName, unhighlight, false)
+});
+
+function highlight(e) {
+    dropArea.classList.add('highlight')
+}
+
+function unhighlight(e) {
+    dropArea.classList.remove('highlight')
+}
+
+dropArea.addEventListener('drop', handleDrop, false);
+
+function handleDrop(e) {
+    let dt = e.dataTransfer;
+    let files = dt.files;
+
+    $('#gallery').empty()
+    $('#message_files')[0].files = files
+
+    handleFiles($('#message_files')[0])
+}
+
+$('#message_files').on('change', function (e) {
+    let my_input = e.target
+
+    handleFiles(my_input)
+})
+
+function handleFiles(my_input) {
+    if (my_input.files.length > 10) {
+        alert('Нельзя загрузить более 10 файлов')
+        my_input.value = ''
+        return
+    }
+
+    for (let i=0; i<my_input.files.length; i++) {
+        let reader = new FileReader()
+        reader.readAsDataURL(my_input.files[i]);
+        reader.onloadend = function () {
+            let imgDiv = document.createElement('div')
+            imgDiv.style.cssText = 'position: relative;'
+            imgDiv.classList.add('img-div')
+
+            let img = document.createElement('img');
+            img.src = reader.result;
+
+            imgDiv.appendChild(img)
+
+            document.getElementById('gallery').appendChild(imgDiv)
+        }
+    }
 }
