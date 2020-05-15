154397f54e8e2a49396c7b62e7234b83e962858c
Кропотухин Алексей <nameies@yandex.ru>
2020-05-15 03:17:19 +0300

    DB changed

--- meta_social/meta_social/settings.py
+++ meta_social/meta_social/settings.py
@@ -14,7 +14,7 @@ import os, sys
 
 # Simple changing DB and redis
 
-START_WITH_DOCKER = False
+START_WITH_DOCKER = True
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
