70ff1f3192a9f1ac1faf13ca9545bbb5fcbce10b
Valber <13department@gmail.com>
2020-05-21 18:18:53 +0300

    Post Create/Delete, Like/Unlike, Comment cvrg>60%

--- meta_social/apps/post/tests.py
+++ meta_social/apps/post/tests.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.models import User
 from django.test import TestCase, Client
 from post.forms import PostForm
-from post.models import Post
+from post.models import Post, Comment
 
 
 class MetaSetUp(TestCase):
@@ -21,12 +21,29 @@ class PostView(MetaSetUp):
     def test_page(self):
         self.assertEqual(self.response.status_code, 200)
 
+    def test_like(self):
+        self.client.post('/like/1/')
+        post = Post.objects.get(id=1)
+        # like
+        self.assertTrue(post.likes.filter(user=self.user))
+        # unlike
+        self.client.post('/like/1/')
+        self.assertFalse(post.likes.filter(user=self.user))
+
+    def test_comment(self):
+        txt = 'Say My Name'
+        self.client.post('/post/1/send_comment/', {'text': txt})
+        self.assertTrue(Comment.objects.get(user=self.user, text=txt))
+
 
 class PostCreate(MetaSetUp):
     def setUp(self):
         super().setUp()
 
     def test_postform(self):
+        """
+        create and delete simple text Post
+        """
         form_data = {'text': "something wrong?"}
         formset_data = {
             'form-TOTAL_FORMS': '0',
@@ -40,3 +57,6 @@ class PostCreate(MetaSetUp):
         p = Post.objects.get(user=self.user, text=form_data['text'])
         response = self.client.get('/post/{}/'.format(p.id))
         self.assertEqual(response.status_code, 200)
+        self.client.post('/post/{}/remove/'.format(p.id))
+        self.assertFalse(Post.objects.filter(user=self.user,
+                                             text=form_data['text']))
