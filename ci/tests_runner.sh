c6eba8a158efe9af0f732d5c9fd70f3ea875e8e5
Кропотухин Алексей <nameies@yandex.ru>
2020-05-27 07:29:21 +0300

    Updated ci tests runner

---
+++ ci/tests_runner.sh
@@ -0,0 +1,7 @@
+python meta_social/manage.py test chat
+python meta_social/manage.py test community
+python meta_social/manage.py test core
+python meta_social/manage.py test friends
+python meta_social/manage.py test music
+python meta_social/manage.py test post
+python meta_social/manage.py test user_profile
