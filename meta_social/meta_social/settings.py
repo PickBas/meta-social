4ab22ecd49f30f7baa1a6b3e597e6a2f89456b59
Кропотухин Алексей <nameies@yandex.ru>
2020-05-27 19:58:35 +0300

    Deleted env

--- meta_social/meta_social/settings.py
+++ meta_social/meta_social/settings.py
@@ -14,7 +14,7 @@ import os
 import sys
 
 # Simple changing DB and redis
-START_WITH_DOCKER = os.getenv('START_WITH_DOCKER', False) == 'True'
+START_WITH_DOCKER = True
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
