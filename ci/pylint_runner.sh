bf7bffcb694d01ed898554d4c2a73a7e1386757d
Сайед Кирилл <sayed.kirill@gmail.com>
2020-05-27 13:44:48 +0300

    Fix pylint (1)

--- ci/pylint_runner.sh
+++ ci/pylint_runner.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-sed -i s/community = models.ForeignKey(to='community.Community', on_delete=models.CASCADE, null=True)/community = models.ForeignKey(to='community.models.Community', on_delete=models.CASCADE, null=True) meta_social/apps/post/models.py
+sed -i "/community = models.ForeignKey(to='community.Community', on_delete=models.CASCADE, null=True)/community = models.ForeignKey(to='community.models.Community', on_delete=models.CASCADE, null=True)" meta_social/apps/post/models.py
 
-sed -i s/owner_community = models.ForeignKey(to='community.Community', on_delete=models.CASCADE, null=True, related_name='owner_community')/owner_community = models.ForeignKey(to='community.models.Community', on_delete=models.CASCADE, null=True, related_name='owner_community') meta_social/apps/post/models.py
+sed -i "/owner_community = models.ForeignKey(to='community.Community', on_delete=models.CASCADE, null=True, related_name='owner_community')/owner_community = models.ForeignKey(to='community.models.Community', on_delete=models.CASCADE, null=True, related_name='owner_community')" meta_social/apps/post/models.py
 
 # run pylint
 pylint $(python ./pylint_runner.py) --load-plugins pylint_django | tee pylint.txt
