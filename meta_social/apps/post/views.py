cb04ab56470ff0000635a1f73823e43504677cbe
Server <server@ourserver.server>
2020-05-28 14:07:04 +0200

    Minor fixes

--- meta_social/apps/post/views.py
+++ meta_social/apps/post/views.py
@@ -243,7 +243,7 @@ class PostViews:
 
         if post.get_images():
             for img in post.get_images():
-                photo = PostImages(post=new_post, image=img.image, from_user_id=post.user)
+                photo = PostImages(post=new_post, image=img.image, from_user_id=post.user.id)
             photo.save()
 
         return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
