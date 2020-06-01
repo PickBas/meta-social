131376cd8648cf8c1d68fb7024bb889386c7027f
Хотеев Сергей <13department@gmail.com>
2020-06-01 14:38:24 +0300

    Add test card

--- meta_social/apps/core/urls.py
+++ meta_social/apps/core/urls.py
@@ -4,6 +4,7 @@ Meta social core urls
 
 from django.urls import path
 from django.contrib.auth.decorators import login_required
+from django.views.generic import TemplateView
 
 from .views import Index, GlobalSearch
 
@@ -12,4 +13,7 @@ urlpatterns = [
     path('', login_required(Index.as_view()), name='home'),
     path('ajax/update_nav/', login_required(Index.update_nav)),
     path('ajax/search/', login_required(GlobalSearch.as_view())),
+    path('about/',
+         TemplateView.as_view(template_name='about_us.html'),
+         name='about'),
 ]
