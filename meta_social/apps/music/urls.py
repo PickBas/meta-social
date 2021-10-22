909f037d5c1c5b0c4075732d73c9b6bd2560b761
Kirill Sayed <sayed.kirill@gmail.com>
2021-10-22 09:41:12 +0300

    Added url names for music

--- meta_social/apps/music/urls.py
+++ meta_social/apps/music/urls.py
@@ -9,9 +9,24 @@ from .views import MusicViews
 
 
 urlpatterns = [
-    path('accounts/profile/<custom_url>/music/', login_required(MusicViews.MusicList.as_view())),
-    path('music/upload/', login_required(MusicViews.MusicUpload.as_view())),
-    path('music/<int:music_id>/add/', login_required(MusicViews.add_music)),
-    path('music/<int:music_id>/add_from_search/', login_required(MusicViews.add_music_from_search)),
-    path('music/<int:music_id>/remove/', login_required(MusicViews.remove)),
+    path(
+        'accounts/profile/<custom_url>/music/',
+        login_required(MusicViews.MusicList.as_view()),
+        name='profile-music-page'),
+    path(
+        'music/upload/',
+        login_required(MusicViews.MusicUpload.as_view()),
+        name='music-upload'),
+    path(
+        'music/<int:music_id>/add/',
+        login_required(MusicViews.add_music),
+        name='profile-music-add'),
+    path(
+        'music/<int:music_id>/add_from_search/',
+        login_required(MusicViews.add_music_from_search),
+        name='profile-music-add-from-search'),
+    path(
+        'music/<int:music_id>/remove/',
+        login_required(MusicViews.remove),
+        name='music-remove'),
 ]
