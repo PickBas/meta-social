115a91e03bcf308a5929b94fd422dbe55af18de6
Kirill Sayed <sayed.kirill@gmail.com>
2021-10-22 09:32:39 +0300

    Added names to urls in user_profile

--- meta_social/apps/user_profile/urls.py
+++ meta_social/apps/user_profile/urls.py
@@ -11,13 +11,34 @@ from .views import ProfileViews
 
 
 urlpatterns = [
-    path('accounts/profile/<str:user_url>/', login_required(ProfileViews.ProfilePage.as_view())),
-    path('accounts/profile/<str:user_url>/edit/', login_required(ProfileViews.EditProfile.as_view())),
-    path('change_avatar/', login_required(ProfileViews.AvatarManaging.as_view())),
-    path('like_marks/', login_required(PostViews.PostUrLikes.as_view())),
-    path('accounts/profile/<int:user_id>/send_req/', login_required(ProfileViews.send_friend_request)),
+    path(
+        'accounts/profile/<str:user_url>/',
+        login_required(ProfileViews.ProfilePage.as_view()),
+        name='profile-page'),
+    path(
+        'accounts/profile/<str:user_url>/edit/',
+        login_required(ProfileViews.EditProfile.as_view()),
+        name='profile-page-edit'),
+    path(
+        'change_avatar/',
+        login_required(ProfileViews.AvatarManaging.as_view()),
+        name='profile-change-avatar'),
+    path(
+        'like_marks/',
+        login_required(PostViews.PostUrLikes.as_view()),
+        name='like-set-mark'),
+    path(
+        'accounts/profile/<int:user_id>/send_req/',
+        login_required(ProfileViews.send_friend_request),
+        name='friend-request-send'),
 
-    path('ajax/set_online/', login_required(ProfileViews.set_online)),
+    path(
+        'ajax/set_online/',
+        login_required(ProfileViews.set_online),
+        name='ajax-set-online-status'),
 
-    path('files/<str:user_url>/', login_required(Files.all_files))
+    path(
+        'files/<str:user_url>/',
+        login_required(Files.all_files),
+        name='profile-files-page')
 ]
