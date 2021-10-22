9dfc36993cf3437900a30c83a6672d204246a787
Kirill Sayed <sayed.kirill@gmail.com>
2021-10-22 09:50:42 +0300

    Added urls name for communities

--- meta_social/apps/core/urls.py
+++ meta_social/apps/core/urls.py
@@ -10,8 +10,8 @@ from .views import Index, GlobalSearch, AboutView
 
 urlpatterns = [
     path('', login_required(Index.as_view()), name='home'),
-    path('ajax/update_nav/', login_required(Index.update_nav)),
-    path('ajax/search/', login_required(GlobalSearch.as_view())),
+    path('ajax/update_nav/', login_required(Index.update_nav), name='nav-update'),
+    path('ajax/search/', login_required(GlobalSearch.as_view()), name='global-search'),
     path('about/', AboutView.as_view(), name='about'),
-    path('about/<int:dev_id>/remove/', AboutView.remove_developer)
+    path('about/<int:dev_id>/remove/', AboutView.remove_developer, name='remove-developer')
 ]
